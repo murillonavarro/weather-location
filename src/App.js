55ed36907497938dee9cacd0e04c5183a1403eba
murillonavarro <42552033+murillonavarro@users.noreply.github.com>
2020-06-28 16:58:07 -0300

    Carregando as informações da api.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import axios from 'axios';
 
 function App() {
   const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  let getWeather = async (lat, long) => {
+    let res = await axios.get("http://api.openweathermap.org/data/2.5/weather", {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
+        lang: 'pt',
+        units: 'metric'
+      }
+    });
+    setWeather(res.data);
+  }
 
   useEffect(()=> {
     navigator.geolocation.getCurrentPosition((position)=> {
-      console.log(position.coords.latitude, position.coords.longitude);
+      getWeather(position.coords.latitude, position.coords.longitude);
       setLocation(true)
     })
   }, [])
@@ -17,17 +31,23 @@ function App() {
         Você precisa habilitar a localização para o aplicativa funcionar.
       </Fragment>
     )
+  } else if (weather == false) {
+    return (
+      <Fragment>
+        Carregando o clima...
+      </Fragment>
+    )
   }  else{
     return (
       <Fragment>
-        <h3>Clima nas suas Coordenadas (Exemplo)</h3>
+        <h3>Clima nas suas Coordenadas ({weather['weather'][0]['description']})</h3>
         <hr/>
         <ul>
-          <li>Temperatura atual: xº</li>
-          <li>Temperatura máxima: xº</li>
-          <li>Temperatura minima: xº</li>
-          <li>Pressão: x hpa</li>
-          <li>Umidade: x%</li>
+          <li>Temperatura atual: {weather['main']['temp']}º</li>
+          <li>Temperatura máxima: {weather['main']['temp_max']}º</li>
+          <li>Temperatura minima: {weather['main']['temp_min']}º</li>
+          <li>Pressão: {weather['main']['pressure']} hpa</li>
+          <li>Umidade: {weather['main']['humidity']}%</li>
         </ul>
       </Fragment>
     );
